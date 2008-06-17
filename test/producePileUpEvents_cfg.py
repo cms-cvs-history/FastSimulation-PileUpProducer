20ab257e24fcf7d01a5620674ee843fb8aabae05
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-06-17 11:27:57 +0000

    Cleaning of python config

--- test/producePileUpEvents_cfg.py
+++ test/producePileUpEvents_cfg.py
@@ -1,12 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("PileUp")
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
+# The number of events to be generated
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(10000)
 )
-process.source = cms.Source("PythiaSource",
+
+# Initialize the random number generator service
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    # This is to initialize the random engine of the source
+    theSource = cms.PSet(
+        initialSeed = cms.untracked.uint32(123456),
+        engineName = cms.untracked.string('TRandom3')
+    )
+)
+
+# Pythia
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.source = cms.Source(
+    "PythiaSource",
     pythiaPylistVerbosity = cms.untracked.int32(0),
     # put here the efficiency of your filter (1. if no filter) dummy
     filterEfficiency = cms.untracked.double(1.0),
@@ -15,7 +28,14 @@ process.source = cms.Source("PythiaSource",
     crossSection = cms.untracked.double(55000000000.0),
     maxEventsToPrint = cms.untracked.int32(2),
     PythiaParameters = cms.PSet(
-        pythiaUESettings = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function', 
+        parameterSets = cms.vstring(
+            'pythiaUESettings', 
+            'pythiaMinBias', 
+            'myParameters'
+        ),
+        # UE settings were taken from Configuration/Sprin08Production/python/PythiaUESetting_cfi.py
+        pythiaUESettings = cms.vstring(
+            'MSTJ(11)=3     ! Choice of the fragmentation function', 
             'MSTJ(22)=2     ! Decay those unstable particles', 
             'PARJ(71)=10 .  ! for which ctau  10 mm', 
             'MSTP(2)=1      ! which order running alphaS', 
@@ -37,14 +57,11 @@ process.source = cms.Source("PythiaSource",
             'PARP(64)=0.2    ! ', 
             'MSTP(91)=1     !', 
             'PARP(91)=2.1   ! kt distribution', 
-            'PARP(93)=15.0  ! '),
-        # This is a vector of ParameterSet names to be read, in this order
-        # The first two are in the include files below
-        # The last one are simply my additional parameters
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'pythiaMinBias', 
-            'myParameters'),
-        pythiaMinBias = cms.vstring('MSEL=0         ! User defined processes', 
+            'PARP(93)=15.0  ! '
+        ),
+        # The processes open for minimum bias events
+        pythiaMinBias = cms.vstring(
+            'MSEL=0         ! User defined processes', 
             'MSUB(11)=1     ! Min bias process', 
             'MSUB(12)=1     ! Min bias process', 
             'MSUB(13)=1     ! Min bias process', 
@@ -55,33 +72,34 @@ process.source = cms.Source("PythiaSource",
             'MSUB(93)=1     ! Min bias process, single diffractive', 
             'MSUB(94)=1     ! Min bias process, double diffractive', 
             'MSUB(95)=1     ! Min bias process'),
-        #    Disable the ctau check 
+        #    Disable the ctau check : particles are not decayed
         myParameters = cms.vstring('PARJ(71) = -1.')
+        # This is a vector of ParameterSet names to be read, in this order
+        # The first two are minbias-event configuration
+        # The last one are simply my additional parameters
     )
 )
 
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-    # this sets the random number seed used by Pythia
-    sourceSeed = cms.untracked.uint32(123456)
-)
-
-process.prodPU = cms.EDFilter("producePileUpEvents",
+# The module to produce events
+process.prodPU = cms.EDFilter(
+    "producePileUpEvents",
     PUParticleFilter = cms.PSet(
-        # Protons with energy larger than EProton (GeV) are all kept
-        EProton = cms.double(5000.0),
         # Particles with |eta| > etaMax (momentum direction at primary vertex) 
         # are not simulated - 7.0 includes CASTOR (which goes to 6.6) 
         etaMax = cms.double(7.0),
         # Charged particles with pT < pTMin (GeV/c) are not simulated
         pTMin = cms.double(0.0),
         # Particles with energy smaller than EMin (GeV) are not simulated
-        EMin = cms.double(0.0)
+        EMin = cms.double(0.0),
+        # Protons with energy larger than EProton (GeV) are all kept
+        EProton = cms.double(5000.0)
     ),
     PUEventFile = cms.untracked.string('MinBiasEvents_001.root'),
     SavePileUpEvents = cms.bool(True),
     BunchPileUpEventSize = cms.uint32(1000)
 )
 
+# The path
 process.p = cms.Path(process.prodPU)
 
 
